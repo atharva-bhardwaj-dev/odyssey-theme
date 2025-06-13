cd7888232f98640e2eeb5aa1152614ab210848a9
atharva-bhardwaj-dev <abhardwaj0624@gmail.com>
2025-06-13 12:06:21 +0530

    Update settings.js

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,8 +1,8 @@
 export default {
-	title: `TIF | Powering the Future of Business with Tech, Finance & Consulting`,
-description: `TIF is your strategic partner for digital transformation — delivering cutting-edge tech, intelligent financial solutions, and business consulting that drives real growth.`,
-url: `https://your-netlify-site.netlify.app`, // Or your custom domain
-name: `TIF`,
-enableThemeSwitcher: true,
-showPlug: false,
+  title: `TIF | Powering the Future of Business with Tech, Finance & Consulting`,
+  description: `TIF is your strategic partner for digital transformation — delivering cutting-edge tech, intelligent financial solutions, and business consulting that drives real growth.`,
+  url: `https://your-netlify-site.netlify.app`, // Or your custom domain
+  name: `TIF`,
+  enableThemeSwitcher: true,
+  showPlug: false,
 };
